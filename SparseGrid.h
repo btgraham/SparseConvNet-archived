dc17da37554ed1868e8506dedbc83cd58ab677bf
Benjamin Thomas Graham <btgraham@gmail.com>
2015-06-05 02:12:10 +0100

    ...

--- SparseGrid.h
+++ SparseGrid.h
@@ -1,9 +1,8 @@
 //We need a hash table:
 //We can either use
 // a) Google's sparsehash dense_hash_map, or
-// b) Boost's unordered_map, or
-// c) the C++11 std::unordered_map
-// Option (a) seems fastest. To speed up (b) or (c), use a "greedy" custom allocator to prevent threads getting in each others way as they access memory to grow the map??
+// b) the C++11 std::unordered_map
+// Option (a) seems fastest. To speed up (b), use a "greedy" custom allocator to prevent threads getting in each others way as they access memory to grow the map??
 
 #define USE_GOOGLE_SPARSEHASH
 
@@ -13,15 +12,7 @@
 #ifdef USE_GOOGLE_SPARSEHASH
 #include <functional>
 #include <google/dense_hash_map>
-struct SparseGridEq
-{
-  bool operator()(const int64_t a, const int64_t b) const
-  {
-    return a==b;
-  }
-};
-typedef google::dense_hash_map<int64_t, int, std::hash<int64_t>, SparseGridEq> SparseGridMap;
-typedef google::dense_hash_map<int64_t, int, std::hash<int64_t>, SparseGridEq>::iterator SparseGridIter;
+typedef google::dense_hash_map<int64_t, int, std::hash<int64_t>, std::equal_to<int64_t> > SparseGridMap;
 
 class SparseGrid {
 public:
@@ -36,16 +27,18 @@ public:
   }
 };
 #else
+//Libraries -ltbb -ltbbmalloc
 #include <unordered_map>
-typedef std::unordered_map<int64_t,int> SparseGridMap;
-typedef std::unordered_map<int64_t,int>::iterator SparseGridIter;
+#include <tbb/scalable_allocator.h>
+//typedef std::unordered_map<int64_t,int,std::hash<int64_t>,std::equal_to<int64_t>,std::allocator< std::pair<const int64_t,int> > > SparseGridMap;
+typedef std::unordered_map<int64_t,int,std::hash<int64_t>,std::equal_to<int64_t>,tbb::scalable_allocator< std::pair<const int64_t,int> > > SparseGridMap;
+
 
 class SparseGrid {
 public:
   int backgroundCol; //Set to -1 when no "null vector" is needed
+  HeapArena ha;
   SparseGridMap mp;
-  SparseGrid() {
-    backgroundCol=-1; //Indicate that no "null vector" is needed
-  }
+  SparseGrid() : ha(1048576), mp(ha), backgroundCol(-1) {}
 };
 #endif
