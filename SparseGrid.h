14227ce569b4ccd1bc53150b3a8f2c64c67e16ce
Benjamin Thomas Graham <btgraham@gmail.com>
2015-06-06 23:58:45 +0100

    ...

--- SparseGrid.h
+++ SparseGrid.h
@@ -2,12 +2,13 @@
 //We can either use
 // a) Google's sparsehash dense_hash_map, or
 // b) the C++11 std::unordered_map
-// Option (a) seems fastest. To speed up (b), use tbb::scalable_alloc (or similar) to prevent threads getting in each others way as they access memory to grow the maps.
+// c) the C++11 std::unordered_map with tbb::scalable_alloc (or similar) to prevent threads getting in each others way as they access memory to grow the maps,
+// d) vectors disguised as a hash map (ok in 2 dimensions)
 
 //#define USE_GOOGLE_SPARSEHASH
-#define USE_VECTOR_HASH
 //#define USE_UNORDERED_MAP
 //#define USE_UNORDERED_MAP_TBB
+#define USE_VECTOR_HASH
 
 
 #pragma once
@@ -31,17 +32,6 @@ public:
 };
 #endif
 
-#ifdef USE_VECTOR_HASH
-#include "vectorHash.h"
-typedef vectorHash SparseGridMap;
-class SparseGrid {
-public:
-int backgroundCol; //Set to -1 when no "null vector" is needed
-SparseGridMap mp;
-SparseGrid() : backgroundCol(-1) {}
-};
-#endif
-
 #ifdef USE_UNORDERED_MAP
 #include <unordered_map>
 typedef std::unordered_map<int64_t,int,std::hash<int64_t>,std::equal_to<int64_t>,std::allocator< std::pair<const int64_t,int> > > SparseGridMap;
@@ -58,6 +48,17 @@ public:
 #include <unordered_map>
 #include <tbb/scalable_allocator.h>
 typedef std::unordered_map<int64_t,int,std::hash<int64_t>,std::equal_to<int64_t>,tbb::scalable_allocator< std::pair<const int64_t,int> > > SparseGridMap;
+class SparseGrid {
+public:
+  int backgroundCol; //Set to -1 when no "null vector" is needed
+  SparseGridMap mp;
+  SparseGrid() : backgroundCol(-1) {}
+};
+#endif
+
+#ifdef USE_VECTOR_HASH
+#include "vectorHash.h"
+typedef vectorHash SparseGridMap;
 class SparseGrid {
 public:
   int backgroundCol; //Set to -1 when no "null vector" is needed
